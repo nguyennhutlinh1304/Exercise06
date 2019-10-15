cc6456f55e9e20553af859904bc4812543e6f61b
hoanglong <nguyenphamhoanglong2000@gmail.com>
2019-10-15 15:27:37 +0700

    Hello

---
+++ Exercise06/Prolem1/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prolem1
+{
+    public class Account
+    {
+        public Account (int amount)
+        {
+
+        }
+        public void Deposit(int amount)
+        {
+
+        }
+        public void Withdraw(int amount)
+        {
+
+        }
+        public int Balance()
+        {
+            return 0;
+
+        }
+    }
+}
---
+++ Exercise06/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prolem1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTestAccount
+    {
+        [TestMethod]
+        public void TestMethodAccount()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance());
+        }
+        [TestMethod]
+        public void TestMethodDeposit()
+        {
+
+        }
+    }
+}
